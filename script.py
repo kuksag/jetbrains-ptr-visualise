3825f8eaa657e5faa791aab8417cc3f8c18a6e3a
kuksag <georgiy.kuksa@gmail.com>
2021-10-24 15:04:41 +0300

    optimized looking through threads

--- script.py
+++ script.py
@@ -5,13 +5,18 @@ import lldb
 PATH_TO_MAPS = '/proc/{}/maps'
 
 
-def get_range(pid, pointer):
-    with open(PATH_TO_MAPS.format(pid), 'r') as maps:
+def get_threads_with_range(process: lldb.SBProcess):
+    threads = sorted(process.threads, key=lambda x: x.frames[0].sp)
+    ranges = []
+    ptr = 0
+    with open(PATH_TO_MAPS.format(process.id), 'r') as maps:
         for line in maps:
             # parse '0x12-0x34 56 78' to (0x12, 0x34) and cast to base 10
             left, right = list(map(lambda x: int(x, 16), line.split()[0].split('-')))
-            if left <= pointer <= right:
-                return left, right
+            if ptr < len(threads) and left <= threads[ptr].frames[0].sp <= right:
+                ranges.append((left, right))
+                ptr += 1
+    return list(zip(threads, ranges))
 
 
 def visualise_pointer(debugger, command, exe_ctx, result, internal_dict):
@@ -19,12 +24,8 @@ def visualise_pointer(debugger, command, exe_ctx, result, internal_dict):
         print('Expected exactly one argument (name of the pointer)', file=result)
         return
     pointer_value = exe_ctx.GetFrame().FindVariable(command).data.uint64.all()[0]
-    pid = exe_ctx.GetProcess().id
-    for thread in exe_ctx.GetProcess():
-        left, right = get_range(pid, thread.frames[0].sp) or (None, None)
-        if (left or right) is None:
-            print("Can't find range for thread #{} in {}".format(thread.idx, PATH_TO_MAPS.format(pid)), file=result)
-        elif left <= pointer_value <= right:
+    for thread, (left, right) in get_threads_with_range(exe_ctx.GetProcess()):
+        if left <= pointer_value <= right:
             for frame in thread.frames:
                 for var in frame.vars:
                     try:
@@ -32,7 +33,9 @@ def visualise_pointer(debugger, command, exe_ctx, result, internal_dict):
                         if location == pointer_value:
                             print('Found {} in thread #{}, frame #{}'.format(pointer_value, thread.idx, frame.idx),
                                   file=result)
+                            return
                     except Exception:
+                        # suppress failed int-16 cast
                         pass
 
 
