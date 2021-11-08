6bec01d992d7190ca22bcf8ffa39f10e75f41a79
kuksag <georgiy.kuksa@gmail.com>
2021-11-08 17:47:07 +0300

    added tests for array

---
+++ tests/array/TestArray.py
@@ -0,0 +1,30 @@
+"""
+Check the many frames case
+"""
+
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test.decorators import *
+
+PATH_TO_SCRIPT = '../../script.py'
+
+
+class TestArray(TestBase):
+    mydir = TestBase.compute_mydir(__file__)
+
+    @no_debug_info_test
+    def test_array(self):
+        self.build()
+        self.main_source_file = lldb.SBFileSpec("main.cpp")
+        self.run_test()
+
+    def run_test(self):
+        self.runCmd(f"command script import {PATH_TO_SCRIPT} --allow-reload")
+
+        (target, process, thread, bkpt) = lldbutil.run_to_source_breakpoint(self,
+                                                                            "Set a breakpoint here",
+                                                                            self.main_source_file)
+
+        self.expect("vp", ordered=False, substrs=['"a" points to object "array[0]", that located in "main"',
+                                                  '"b" points to object "array[1]", that located in "main"',
+                                                  '"c" points to object "array[2]", that located in "main"',
+                                                  '"d" points to object "array[3]", that located in "main"'])
---
+++ tests/array/main.cpp
@@ -0,0 +1,8 @@
+int main() {
+    int array[4];
+    int *a = &array[0];
+    int *b = &array[1];
+    int *c = &array[2];
+    int *d = &array[3];
+    return 0; // Set a breakpoint here
+}
