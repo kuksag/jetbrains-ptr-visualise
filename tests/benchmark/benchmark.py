a1922a86b791fb8712a108fe030f267328504c30
kuksag <georgiy.kuksa@gmail.com>
2021-11-29 12:07:31 +0300

    debug fix in log

--- tests/benchmark/benchmark.py
+++ tests/benchmark/benchmark.py
@@ -16,14 +16,12 @@ def build():
 def inspect_log():
     times = defaultdict(list)
     with open(LOG_PATH, 'r') as log:
-        # Inspect only last TESTS runs
-        run = 1
         for line in reversed(log.readlines()):
             if 'script_started' in line:
-                run += 1
-                if run == TESTS:
-                    break
-            if 'trace_pointer' not in line and 'visualise_pointers' not in line:
+                break
+            if 'DEBUG' in line:
+                continue
+            if not any(match in line for match in ['visualise_pointers', 'trace_pointer']):
                 continue
             func, duration = line.split(':')[2].split()
             times[func].append(float(duration))
@@ -34,7 +32,7 @@ if __name__ == '__main__':
     build()
     for test in range(TESTS):
         run_debugger.run()
-    logging.info('benchmark_started %s', datetime.now())
-    logging.info('benchmark_runs %s', TESTS)
+    logging.debug('benchmark_started %s', datetime.now())
+    logging.debug('benchmark_runs %s', TESTS)
     for func, durations in inspect_log().items():
-        logging.info(f'mean_time_{func} {sum(durations) / len(durations)}')
+        logging.debug(f'mean_time_{func} {sum(durations) / len(durations)}')
