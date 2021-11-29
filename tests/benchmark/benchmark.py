b752dd8c8e09c231520d78571328f672d741c9d2
kuksag <georgiy.kuksa@gmail.com>
2021-11-29 11:43:51 +0300

    logger fixes

--- tests/benchmark/benchmark.py
+++ tests/benchmark/benchmark.py
@@ -1,7 +1,8 @@
-import os
+from datetime import datetime
 import subprocess
 import run_debugger
 from collections import defaultdict
+import logging
 
 LOG_PATH = 'execution_time.log'
 TESTS = 5
@@ -12,17 +13,16 @@ def build():
     subprocess.run(['make'])
 
 
-def remove_previous_log():
-    try:
-        os.remove(LOG_PATH)
-    except FileNotFoundError:
-        pass
-
-
 def inspect_log():
     times = defaultdict(list)
     with open(LOG_PATH, 'r') as log:
-        for line in log:
+        # Inspect only last TESTS runs
+        run = 1
+        for line in reversed(log.readlines()):
+            if 'script_started' in line:
+                run += 1
+                if run == TESTS:
+                    break
             if 'trace_pointer' not in line and 'visualise_pointers' not in line:
                 continue
             func, duration = line.split(':')[2].split()
@@ -31,9 +31,10 @@ def inspect_log():
 
 
 if __name__ == '__main__':
-    remove_previous_log()
     build()
     for test in range(TESTS):
         run_debugger.run()
+    logging.info('benchmark_started %s', datetime.now())
+    logging.info('benchmark_runs %s', TESTS)
     for func, durations in inspect_log().items():
-        print(f'{func}: {sum(durations) / len(durations)}')
+        logging.info(f'mean_time_{func} {sum(durations) / len(durations)}')
--- time_logger.py
+++ time_logger.py
@@ -1,18 +1,17 @@
-import datetime
+from datetime import datetime
 from functools import wraps
 import logging
-import datetime
 
 logging.basicConfig(filename='execution_time.log', level=logging.DEBUG, filemode='a')
-logging.info('script_started: %s', datetime.datetime.now())
+logging.info('script_started %s', datetime.now())
 
 
 def log_time(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        start = datetime.datetime.now()
+        start = datetime.now()
         result = func(*args, **kwargs)
-        end = datetime.datetime.now()
+        end = datetime.now()
         logging.info('%s %s', func.__name__, (end - start).microseconds / 10 ** 6)
         return result
 
