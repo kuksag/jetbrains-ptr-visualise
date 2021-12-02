954fabd19871316f97c724c07a207153457832b9
kuksag <georgiy.kuksa@gmail.com>
2021-12-03 00:07:42 +0300

    Updated pointer-resolve algorithm

--- script.py
+++ script.py
@@ -1,61 +1,12 @@
 #!/usr/bin/env python
 
 import lldb
-import attr
+import json
 from time_logger import log_time
 
 PATH_TO_MAPS = '/proc/{}/maps'
-OUTPUT_PATTERN = '"{ptr_name}" points to object "{obj_name}", that located in "{func_name}", frame #{frame_id}, ' \
+OUTPUT_PATTERN = '"{ptr_name}" points to "{obj_name}", that located in "{func_name}", frame #{frame_id}, ' \
                  'of thread #{thread_id} '
-
-
-def read_location(location):
-    """
-    Trying to cast location from "proc/{PID}/maps" to int or None
-    e.g 7f000d417000 -> int(...)
-    We need this function because not all var.location returns correct value
-    """
-    try:
-        return int(location, 16)
-    except ValueError:
-        return None
-
-
-@attr.s()
-class TraceInfo:
-    """
-    Holds info after calling trace_var(pointer, var)
-    Can build a name of the target-object
-    """
-    thread = attr.ib(validator=attr.validators.instance_of(lldb.SBThread))
-    frame = attr.ib(validator=attr.validators.instance_of(lldb.SBFrame))
-    trace = attr.ib(validator=attr.validators.instance_of(list))
-    pointer = attr.ib(validator=attr.validators.instance_of(int))
-
-    def get_offset(self):
-        offset = self.pointer - read_location(self.trace[-1].location)
-        assert offset >= 0
-        return offset
-
-    def build_name_from_trace(self):
-        result = ''
-        for var in self.trace:
-            result += var.name
-            if var.num_children > 0 and not var.type.IsArrayType() and var != self.trace[-1]:
-                result += '.'
-        return result
-
-    @staticmethod
-    def get_info(trace_info, ptr_name=None):
-        if trace_info:
-            return OUTPUT_PATTERN.format(ptr_name=ptr_name or trace_info.pointer,
-                                         obj_name=trace_info.build_name_from_trace() + (
-                                             f" (offset +{trace_info.get_offset()})" if trace_info.get_offset() else ""),
-                                         func_name=trace_info.frame.name,
-                                         frame_id=trace_info.frame.idx,
-                                         thread_id=trace_info.thread.idx)
-        else:
-            return f'target not found for'
 
 
 def get_threads_with_ranges(process: lldb.SBProcess):
@@ -78,33 +29,68 @@ def get_threads_with_ranges(process: lldb.SBProcess):
     return threads, ranges
 
 
-def trace_var(pointer, var: lldb.SBValue, pointee_type: lldb.SBType = None, allow_padding=False):
+class PointerMatch:
+    def __init__(self):
+        self.matches = [[], [], []]
+        self.thread = None
+        self.frame = None
+        self.pointer = None
+
+    def extend(self, other):
+        self.matches[0].extend(other.matches[0])
+        self.matches[1].extend(other.matches[1])
+        self.matches[2].extend(other.matches[2])
+
+    def _get_info_from_matches(self):
+        return [list(map(lambda var: var.path, self.matches[0])),
+                list(map(lambda var: var.path, self.matches[1])),
+                list(map(lambda var: (var.path, self.pointer - int(var.location, 16)), self.matches[2]))]
+
+    def to_json(self, ptr_name=None):
+        payload = {
+            'frame_id': self.frame.idx,
+            'frame_name': self.frame.name,
+            'matches': self._get_info_from_matches(),
+            'ptr_name': ptr_name,
+            'thread_id': self.thread.idx,
+        }
+        return json.dumps(payload, sort_keys=True, indent=4)
+
+    def to_plain(self, ptr_name=None):
+        plain_matches = self._get_info_from_matches()
+        match = plain_matches[0] or plain_matches[1] or list(
+            map(lambda x: f'{x[0]} + {hex(x[1])}', plain_matches[2]))
+        if not match:
+            return 'Not found'
+        if len(match) == 1:
+            match = match[0]
+        return OUTPUT_PATTERN.format(ptr_name=ptr_name or hex(self.pointer),
+                                     obj_name=match,
+                                     func_name=self.frame.name,
+                                     frame_id=self.frame.idx,
+                                     thread_id=self.thread.idx)
+
+
+def trace_var(pointer, var: lldb.SBValue, pointee_type: lldb.SBType = None):
     """
     If var is not a struct or array, then return it.
     Otherwise, look through all members of var and go inside at which pointer points.
     """
-    if var.num_children > 0 and not var.type.is_pointer and var.type != pointee_type:
-        for child in var.children:
-            location = read_location(child.location)
-            if not location:
-                continue
-            try:
-                if location <= pointer < location + child.size:
-                    return [var] + trace_var(pointer, child, pointee_type)
-            except RuntimeError:
-                # The wrong type was chosen; e.g. union
-                pass
-            except AssertionError:
-                # Gotta find object without offset
-                pass
-        # Didnt find any corresponding member => the pointee is in the padding
-        if not allow_padding:
-            assert pointer == read_location(var.location)
-        return [var]
+    result = PointerMatch()
+    if pointee_type == var.type and pointer == int(var.location, 16):
+        result.matches[0].append(var)
+    elif pointer == int(var.location, 16):
+        result.matches[1].append(var)
     else:
-        if pointee_type is not None and pointee_type != var.type:
-            raise RuntimeError(f"pointer type {pointee_type} doesn't match with found type {var.type}")
-        return [var]
+        result.matches[2].append(var)
+
+    for i in range(var.num_children):
+        child = var.GetChildAtIndex(i)
+        location = int(child.location, 16)
+        if location <= pointer < location + child.size:
+            result.extend(trace_var(pointer, child, pointee_type))
+
+    return result
 
 
 def get_thread_for_pointer(pointer, threads, ranges):
@@ -140,25 +126,18 @@ def trace_pointer(pointer, process: lldb.SBProcess, pointee_type: lldb.SBType = 
     """
     Finding thread, stack, function, object to which the pointer points
     """
-    if pointee_type and pointee_type.name == 'void':
-        pointee_type = None
     thread = get_thread_for_pointer(pointer, *get_threads_with_ranges(process))
     if thread:
         frame = get_frame_for_pointer(pointer, thread)
         if frame:
             for var in frame.GetVariables(True, True, False, True):
-                location = read_location(var.location)
-                if not location:
-                    continue
+                location = int(var.location, 16)
                 if location <= pointer < location + var.size:
-                    try:
-                        trace = trace_var(pointer, var, pointee_type)
-                    except AssertionError:
-                        trace = trace_var(pointer, var, pointee_type, allow_padding=True)
-                    return TraceInfo(thread=thread,
-                                     frame=frame,
-                                     pointer=pointer,
-                                     trace=trace)
+                    trace = trace_var(pointer, var, pointee_type)
+                    trace.thread = thread
+                    trace.frame = frame
+                    trace.pointer = pointer
+                    return trace
     return None
 
 
@@ -166,9 +145,9 @@ def tp(debugger, command, exe_ctx, result, internal_dict):
     """
     Alias for call from debugger
     """
-    # int(command) -- handles both cases: int and hex
-    trace_info = trace_pointer(int(command), exe_ctx.GetProcess())
-    print(TraceInfo.get_info(trace_info), file=result)
+    # TODO: handle hex
+    pointer_match = trace_pointer(int(command), exe_ctx.GetProcess())
+    print(pointer_match.to_plain(), file=result)
 
 
 @log_time
@@ -179,8 +158,8 @@ def visualise_pointers(debugger, command, exe_ctx, result, internal_dict):
     pointers = list(filter(lambda x: x.type.is_pointer, exe_ctx.GetFrame().GetVariables(True, True, False, True)))
     pointers = list(map(lambda x: (x.data.uint64s[0], x.GetName(), x.type.GetPointeeType()), pointers))
     for pointer, ptr_name, pointee_type in pointers:
-        trace_info = trace_pointer(pointer, exe_ctx.GetProcess(), pointee_type)
-        print(TraceInfo.get_info(trace_info, ptr_name), file=result)
+        pointer_match = trace_pointer(pointer, exe_ctx.GetProcess(), pointee_type)
+        print(pointer_match.to_plain(ptr_name=ptr_name), file=result)
 
 
 def __lldb_init_module(debugger, internal_dict):
